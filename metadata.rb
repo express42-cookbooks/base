86ecb1f43f87ccfaaf892ba1337691481d007747
Ivan Evtukhovich <evtuhovich@gmail.com>
2014-04-23 10:55:09 +0400

    Base cookbook, first public version

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             "base"
+maintainer       "LLC Express 42"
+maintainer_email "info@express42.com"
+license          "MIT"
+description      "Installs/Configures base"
+version          "0.1.2"
