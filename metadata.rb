3ae96f8716c08de357e357d075a8280a5bc5a202
Sergey Bogatyrets <serj.sj@gmail.com>
2014-11-05 12:52:02 +0300

    Aptly

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,9 @@ name             'base'
 maintainer       'LLC Express 42'
 maintainer_email 'info@express42.com'
 license          'MIT'
-description      'Installs/Configures base'
-version          '0.1.2'
+description      'Installs/Configures base hardening recipes'
+version          '0.1.3'
+
+%w( cron lvm nginx aptly ).each do |pkg|
+  depends pkg
+end
